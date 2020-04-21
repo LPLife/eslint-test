f03f2548b8847d28d9df83ce98f99d737da6a1c5
lupei <lupei@linghit.com>
2020-04-21 19:18:11 +0800

    vue-cli3-eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,14 +1,24 @@
 module.exports = {
-  root: true,
-  env: {
-    node: true
-  },
-  extends: ["plugin:vue/essential", "eslint:recommended", "@vue/prettier"],
-  parserOptions: {
-    ecmaVersion: 2020
-  },
-  rules: {
-    "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
-    "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off"
-  }
+    root: true,
+    env: {
+        browser: true,
+        node: true
+    },
+    extends: ['plugin:vue/essential', 'eslint:recommended', '@vue/prettier'],
+    parserOptions: {
+        ecmaVersion: 2020
+    },
+    rules: {
+        'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+        'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+        semi: ['warn', 'always'],
+        quotes: ['warn', 'single'],
+        indent: [
+            'warn',
+            4,
+            {
+                SwitchCase: 1
+            }
+        ]
+    }
 };
